f85730b5619e3ccce544bdff522c8d4a51e8a67f
Puk3p <gheorghe.lupu2@student.tuiasi.ro>
2025-11-09 20:26:24 +0200

    Implement user repository and mapper for user entity conversion

--- backend/src/main/java/moodlev2/infrastructure/persistence/jpa/SpringDataUserRepository.java
+++ backend/src/main/java/moodlev2/infrastructure/persistence/jpa/SpringDataUserRepository.java
@@ -1,4 +1,12 @@
 package moodlev2.infrastructure.persistence.jpa;
 
 
-public interface SpringDataUserRepository {}
+import moodlev2.infrastructure.persistence.jpa.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SpringDataUserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
--- backend/src/main/java/moodlev2/infrastructure/persistence/jpa/UserRepositoryAdapter.java
+++ backend/src/main/java/moodlev2/infrastructure/persistence/jpa/UserRepositoryAdapter.java
@@ -1,5 +1,41 @@
 package moodlev2.infrastructure.persistence.jpa;
 
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import moodlev2.domain.user.User;
+import moodlev2.domain.user.ports.UserRepositoryPort;
+import moodlev2.infrastructure.mapper.UserMapper;
+import org.springframework.stereotype.Component;
 
-public class UserRepositoryAdapter{
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Component
+public class UserRepositoryAdapter implements UserRepositoryPort {
+    private final SpringDataUserRepository springDataUserRepository;
+    private final UserMapper userMapper;
+
+    @Override
+    public Optional<User> findById(Long id) {
+        return springDataUserRepository.findById(id)
+                .map(userMapper::toDomain);
+    }
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        return springDataUserRepository.findByEmail(email)
+                .map(userMapper::toDomain);
+    }
+
+    @Override
+    public boolean existsByEmail(String email) {
+        return springDataUserRepository.existsByEmail(email);
+    }
+
+    @Override
+    public User save(User user) {
+        var entity = userMapper.toEntity(user);
+        var savedEntity = springDataUserRepository.save(entity);
+        return userMapper.toDomain(savedEntity);
+    }
 }
