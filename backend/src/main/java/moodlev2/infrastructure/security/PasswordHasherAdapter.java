4968d5db229fd5c6ed95ca3dea1206bb904a3732
Puk3p <gheorghe.lupu2@student.tuiasi.ro>
2025-11-09 20:30:45 +0200

    Implement PasswordHasherAdapter for password hashing and matching

--- backend/src/main/java/moodlev2/infrastructure/security/PasswordHasherAdapter.java
+++ backend/src/main/java/moodlev2/infrastructure/security/PasswordHasherAdapter.java
@@ -1,6 +1,22 @@
 package moodlev2.infrastructure.security;
 
+import lombok.RequiredArgsConstructor;
+import moodlev2.domain.user.ports.PasswordHasherPort;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+@RequiredArgsConstructor
 @Component
-public class PasswordHasherAdapter{}
+public class PasswordHasherAdapter implements PasswordHasherPort {
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public String hash(String rawPassword) {
+        return passwordEncoder.encode(rawPassword);
+    }
+
+    @Override
+    public boolean matches(String rawPassword, String hashedPassword) {
+        return passwordEncoder.matches(rawPassword, hashedPassword);
+    }
+}
