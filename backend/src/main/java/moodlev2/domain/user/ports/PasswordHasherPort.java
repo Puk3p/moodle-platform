74017623e3225aa628af95ab3bc4579227ad83ca
Puk3p <gheorghe.lupu2@student.tuiasi.ro>
2025-11-09 20:06:00 +0200

    Add password hashing and token service interfaces with methods for user authentication

--- backend/src/main/java/moodlev2/domain/user/ports/PasswordHasherPort.java
+++ backend/src/main/java/moodlev2/domain/user/ports/PasswordHasherPort.java
@@ -1,3 +1,6 @@
 package moodlev2.domain.user.ports;
 
-public interface PasswordHasherPort { }
+public interface PasswordHasherPort {
+    String hash(String rawPassword); //primeste parola si intoarace hash
+    boolean matches(String rawPassword, String hashedPassword); //verifica daca parola corespunde hashului
+}
